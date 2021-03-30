f7d3c23491ad87c5b67c6ae9e0597c2dbc7a8e5d
Gabriel Rios <grios0311@gmail.com>
2021-03-29 21:29:07 -0300

    first commit

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,28 @@
+import { ISpecificationsRepositor } from "../../repositories/ISpecificationsRepository"
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationUseCase {
+    constructor(private specificationsRepository: ISpecificationsRepositor){
+        
+    }
+
+
+    execute({name, description} : IRequest): void{
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if(specificationAlreadyExists){
+            throw new Error("Specification already exists!")
+        }
+        this.specificationsRepository.create({
+            name,
+            description
+        })
+
+    }
+}
+
+export { CreateSpecificationUseCase }
