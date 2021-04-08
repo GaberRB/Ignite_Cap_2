0f14c4fc546730010dce2d12623e8961404339a3
Gabriel Rios <grios0311@gmail.com>
2021-04-07 23:33:05 -0300

    Refatorando Model, Alterando Repo

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../entities/Specification";
 import { ISpecificationsRepositor, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 
